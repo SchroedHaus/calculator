1174a6650725cca743f43bf7e30bba0529e4e139
SchroedHaus <matt.schroeder@pm.me>
2023-11-14 16:48:38 +0000

    Update JS functions and add CSS.

--- js-file.js
+++ js-file.js
@@ -6,111 +6,95 @@ function operate(x, operator, y) {
 
     switch(operator) {
         case '+':
-            answer = add(x, y);
+            answer = x + y;
             break;
         case '-':
-            answer = subtract(x, y);
+            answer = x - y;
             break;
         case 'X':
-            answer = multiply(x, y);
+            answer = x * y;
             break;
         case '/':
-            answer = divide(x, y);
+            if (y == 0) {
+                answer = "Come on, bruh."
+            }
+            else {
+                answer = x / y;
+            }
             break;
     }
-    console.log("Answer: " + answer);
-    return answer;
+    return Math.round(answer * 10000) / 10000;
 }
 
-function add(x, y) {
-    return (x + y);
-}
-
-function subtract(x, y) {
-    return (x - y);
-}
-
-function multiply(x, y) {
-    return (x * y);
-}
-
-function divide(x, y) {
-    return (x / y);
-}
-
+// Create the variables
 let firstNumber = '';
+let secondNumber = '';
 let prevOperator = '';
 let currOperator = ''
-let secondNumber = '';
 let isFirst = true;
 
 function buildFirstNumber(number) {
     firstNumber = firstNumber.concat(number);
     display(firstNumber);
-    console.log("First Number: " + firstNumber);
 }
 
 function buildSecondNumber(number) {
     secondNumber = secondNumber.concat(number);
     display(secondNumber);
-    console.log("Second Number: " + secondNumber);
 }
 
 // Listen for button clicks
 document.querySelector('.container').addEventListener("click", (event) => {
-    //If it's a number button...
-    if (event.target.className === 'number') {
-        let number = event.target.innerHTML;
-        // If it's the first number...
-        if (isFirst == true) {
-            buildFirstNumber(number);
-        }
-        // If it's the second number...
-        else {
-            buildSecondNumber(number);
-        }
-    }
-    // If it's the operator button...
-    else if (event.target.className === 'operator') {
-        currOperator = event.target.innerHTML;
-        // Calulate the firstNumber and listen for the new secondNumber
-        
-        display(currOperator);
+    let buttonType = event.target.className;
 
-        if (!secondNumber) {
-            isFirst = false;
-        }
-        else {
-            firstNumber = operate(firstNumber, prevOperator, secondNumber);
-            console.log("New First Number: " + firstNumber);
+    //If it's a number button...
+    switch(buttonType) {
+        case 'number':
+            let number = event.target.innerHTML;
+            // If it's the first number...
+            if (isFirst == true) {
+                buildFirstNumber(number);
+            }
+            // If it's the second number...
+            else {
+                buildSecondNumber(number);
+            }
+            break;
+
+        case 'operator':
+            currOperator = event.target.innerHTML;
+            // Calulate the firstNumber and listen for the new secondNumber
+            if (!secondNumber) {
+                isFirst = false;
+            }
+            else {
+                firstNumber = operate(firstNumber, prevOperator, secondNumber);
+                secondNumber = '';
+                display(firstNumber);
+            }
+            prevOperator = currOperator;
+            break;
+
+        case 'equals':
+            if (!secondNumber) {
+                display(firstNumber);
+            }
+            else {
+                display(operate(firstNumber, currOperator, secondNumber));
+            }
+            break;
+
+        case 'clear':
+            firstNumber = '';
             secondNumber = '';
-        }
-        prevOperator = currOperator;
-    }
-    else if (event.target.id === 'equals') {
-        display(operate(firstNumber, currOperator, secondNumber));
-    }
-    else if (event.target.id === 'clear') {
-        firstNumber = '';
-        secondNumber = '';
-        currOperator = '';
-        prevOperator = ''
-        isFirst = true;
-        display(0)
+            currOperator = '';
+            prevOperator = ''
+            isFirst = true;
+            display(0)
+            break;
     }
 });
 
 function display(text) {
     document.querySelector('#display').innerHTML = text;
-    console.log("Display:" + text);
 }
-
-// Input firstNumber
-// Input operator
-// Input secondNumber
-// Input operator
-// Calculate
-// Replace firstNumber with calculation
-// Clear secondNumber
-// Input new secondNumber
-// Hit equals - get the answer
